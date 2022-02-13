5e4a1f51bf934a1797e534611fb27cb025ea5620
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2022-02-13 15:25:31 -0300

    expose ids for categories

--- src/main/java/com/luv2code/ecommerce/entity/Product.java
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -12,41 +12,42 @@ import java.util.Date;
 @Table(name="product")
 @Data
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
+    @Column(name = "id")
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name="category_id", nullable = false)
+    @JoinColumn(name = "category_id", nullable = false)
     private ProductCategory category;
 
-    @Column(name="sku")
+    @Column(name = "sku")
     private String sku;
 
-    @Column(name="name")
+    @Column(name = "name")
     private String name;
 
-    @Column(name="description")
+    @Column(name = "description")
     private String description;
 
-    @Column(name="unit_price")
+    @Column(name = "unit_price")
     private BigDecimal unitPrice;
 
-    @Column(name="image_url")
+    @Column(name = "image_url")
     private String imageUrl;
 
-    @Column(name="active")
+    @Column(name = "active")
     private boolean active;
 
-    @Column(name="units_in_stock")
+    @Column(name = "units_in_stock")
     private int unitsInStock;
 
-    @Column(name="date_created")
+    @Column(name = "date_created")
     @CreationTimestamp
     private Date dateCreated;
 
-    @Column(name="last_updated")
+    @Column(name = "last_updated")
     @UpdateTimestamp
     private Date lastUpdated;
 }
