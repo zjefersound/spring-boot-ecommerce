2cd0f0c9b699e798b29067ab01aa09520e2eff55
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2022-04-19 14:51:55 -0300

    checkout and purchase done

---
+++ src/main/java/com/luv2code/ecommerce/dao/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
---
+++ src/main/java/com/luv2code/ecommerce/dto/PurchaseResponse.java
@@ -0,0 +1,8 @@
+package com.luv2code.ecommerce.dto;
+
+import lombok.Data;
+
+@Data
+public class PurchaseResponse {
+    private final String orderTrackingNumber;
+}
